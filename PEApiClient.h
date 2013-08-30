1d9d6bc1f7eaed94965252f064fb617a175ea40f
BliBly <admin@tengfei.biz>
2013-08-30 12:23:42 +0800

    add file

---
+++ PEApiClient.h
@@ -0,0 +1,18 @@
+//
+//  PApiClient.h
+//  saeapns
+//
+//  Created by Eric X.u on 08/06/2013.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import "AFNetworking.h"
+#import "TFApiUrls.h"
+
+@interface PEApiClient : AFHTTPClient
+
++ (PEApiClient *)sharePApiClient;
+
+- (NSInteger)loginWithIdentity:(NSString *)identity password:(NSString *)passwd;
+@end
